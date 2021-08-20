626fc5313672bc092fff852d0a9337f09d70a548
Manoj-Paramsetti <paramsetti.manoj@gmail.com>
2021-08-20 15:24:36 +0530

    Automated with python

---
+++ drlinux/listpkg.py
@@ -0,0 +1,59 @@
+import os	
+
+
+def main_logo():
+	### Colors
+	normal = "\033[0;0;0m"
+	underlined_text = "\033[2;37;40m"
+	green = "\033[1;32;40m"
+
+
+
+	os.system("clear")
+	print('''
+	 M""""""'YMM             M""MMMMMMMM oo                            
+	 M  mmmm. `M             M  MMMMMMMM                               
+	 M  MMMMM  M 88d888b.    M  MMMMMMMM dP 88d888b. dP    dP dP.  .dP 
+	 M  MMMMM  M 88'  `88    M  MMMMMMMM 88 88'  `88 88    88  `8bd8'  
+	 M  MMMM' .M 88       dP M  MMMMMMMM 88 88    88 88.  .88  .d88b.  
+	 M       .MM dP       88 M         M dP dP    dP `88888P' dP'  `dP 
+	 MMMMMMMMMMM             MMMMMMMMMMM                               
+	''')
+
+	try:
+		os.system("./drlinux/blink")
+	except:
+		os.system("chmod +x drlinux/blink")
+		os.system("chmod +x drlinux/offblink")
+
+	print(green, "                  Created by Manoj Paramsetti                     ")
+	print(normal)
+
+### Colors
+normal = "\033[0;0;0m"
+underlined_text = "\033[2;37;40m"
+green = "\033[1;32;40m"
+
+
+def showpkg(packages):
+	length = len(packages)
+	os.system("./drlinux/offblink")
+	
+	for i in range(0, length):
+		print(f"{i+1}. {packages[i]}")
+	n = int(input(green+"\nEnter your option number"+normal+"\n>>>")) - 1
+	return n 
+
+def showfile(packages, n):
+	
+	files = os.listdir('./'+packages[n])
+	length = len(files)
+
+
+	for i in range(0, length):
+		print(f"{i+1}. {files[i].capitalize().replace('_', ' ').replace('.sh','')}")
+	
+	file = int(input(green+"\nEnter your option number"+normal+"\n>>>"))
+	
+	os.system("chmod +x "+packages[n]+"/"+files[file-1])
+	os.system("./"+packages[n]+"/"+files[file-1])
---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import drlinux.listpkg as listpkg
+
+if __name__ == '__main__':
+
+
+	packages = os.listdir('.')	
+	print(packages)
+	ignore = ['main.py', 'LICENSE', 'README.md', '.git', 'drlinux']
+	for i in ignore:
+		packages.remove(i)
+
+
+	listpkg.main_logo()
+	
+	n = listpkg.showpkg(packages)
+	
+	listpkg.main_logo()
+	
+	listpkg.showfile(packages, n)
