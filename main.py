6a320d174ce6bb3e5781f42ba5c8ad2c6c7eca50
Manoj-Paramsetti <paramsetti.manoj@gmail.com>
2021-08-20 15:35:50 +0530

    Code formatted using black

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import os
 import drlinux.listpkg as listpkg
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
+    packages = os.listdir(".")
+    print(packages)
+    ignore = ["main.py", "LICENSE", "README.md", ".git", "drlinux"]
+    for i in ignore:
+        packages.remove(i)
 
-	packages = os.listdir('.')	
-	print(packages)
-	ignore = ['main.py', 'LICENSE', 'README.md', '.git', 'drlinux']
-	for i in ignore:
-		packages.remove(i)
+    listpkg.main_logo()
 
+    n = listpkg.showpkg(packages)
 
-	listpkg.main_logo()
-	
-	n = listpkg.showpkg(packages)
-	
-	listpkg.main_logo()
-	
-	listpkg.showfile(packages, n)
+    listpkg.main_logo()
+
+    listpkg.showfile(packages, n)
