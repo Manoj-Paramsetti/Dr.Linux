9f45d12ca80d07814079f0a334aa3f3797c5c738
Manoj Paramsetti <39455174+Manoj-Paramsetti@users.noreply.github.com>
2021-08-20 13:31:08 +0530

    Create path_not_added.sh
    Error fix: WARNING: The script LIBRARY is installed in '$HOME/.local/bin' which is not on PATH.
      Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.

---
+++ PIP/path_not_added.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ['no_sudo', 'write']
+
+echo -e "\n"
+echo "# Exporting pip library binary files"
+echo -e "\n"
+echo export PATH=$HOME"/.local/bin/":$PATH >> $HOME/.bashrc
+
+echo "PATH is updated"
+echo "Issue solved"
